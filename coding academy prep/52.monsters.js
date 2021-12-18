d1a5d9281b268c4d7e549b58936a19b8a6712b4b
tal0311 <tal.amit0311@gmail.com>
2021-12-18 15:37:58 +0200

    52. monters

--- coding academy prep/52.monsters.js
+++ coding academy prep/52.monsters.js
@@ -19,41 +19,75 @@
 // parents power. The name is the beginning half of the first parent name, and the
 // second half is the end of the second parent name.
 
-// class Monster {
-//   constructor(name, id, power) {
-//     ;(this.name = name), (this.id = id), (this.power = power)
-//   }
-// }
-let name1 = 'first'
+class Monster {
+  constructor(name, id, power) {
+    ;(this.name = 'testMon' || prompt('monster name?')),
+      (this.id = id),
+      (this.power = power)
+  }
+}
 
 let monsters = []
-function createMonster(name) {
+function createMonster() {
   id = 0
+  power = 30
   for (let i = 0; i < 4; i++) {
-    let monster = new Object()
-    monster.name = name
-    monster.id = id++
-    monster.power = 40
+    let monster = new Monster(name, id++, (power += 10))
     monsters.push(monster)
   }
   return monsters
 }
-console.log(monsters)
+console.log(createMonster())
 
-console.log(createMonster(name1))
-
-function getMonsterB(monsters, id) {
-  console.log(monsters)
+function getMonsterById(id) {
   let monster = monsters.filter((monster) => monster.id == id)
   return monster
 }
+// console.log('get Monster by id:', getMonsterById(3))
 
-console.log(getMonsterB(monsters, (id = 3)))
-
-function deleteMonster(monsters, id) {
+function deleteMonster(id) {
   // monsters = monsters.filter((monster) => monster.id !== id)
   monsters.splice(id, id)
 
-  return console.log(monsters)
+  return monsters
 }
-deleteMonster(monsters, 2)
+console.log('delete monster by id:', deleteMonster())
+
+function updateMonster(id, newPower) {
+  let monster = monsters.filter((monster) => monster.id == id)
+  monster[0].power = newPower
+  return monster
+}
+console.log(updateMonster(2, 89))
+
+function findStrongestMonster() {
+  strongest = 0
+  strongestId = 0
+  monsters.forEach((monster) => {
+    if (monster.power > strongest) {
+      strongest = monster.power
+      strongestId = monster.id
+    }
+  })
+
+  return `Monster no.${strongestId} is the strongest monster with a power of ${strongest}`
+}
+console.log(findStrongestMonster(monsters))
+
+console.log('************************************************')
+
+// d. Write the function: breedMonsters(monsterId1, monsterId2), the function
+// returns a new monster. The breeded monster power is an average of its
+// parents power. The name is the beginning half of the first parent name, and the
+// second half is the end of the second parent name.
+
+function breedMonsters({ ...mon1 }, { ...mon2 }) {
+  mon1 = mon1[0]
+  mon2 = mon2[0]
+  name = mon1.name.slice(0, 2) + mon2.name.slice(3, -1)
+  id = mon2.id + mon1.id
+  power = (mon1.power + mon2.power) / 2
+  breadedMonster = new Monster(name, id, power)
+  return breadedMonster
+}
+console.log(breedMonsters(getMonsterById(1), getMonsterById(2)))
