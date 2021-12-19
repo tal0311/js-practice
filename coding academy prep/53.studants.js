bd6e8ba25f86296efac5ee7a6cf9984cacc246eb
tal0311 <tal.amit0311@gmail.com>
2021-12-19 15:38:37 +0200

    students

--- coding academy prep/53.studants.js
+++ coding academy prep/53.studants.js
@@ -14,7 +14,11 @@ class Student {
   }
 }
 
-let studentOne = new Student('test-student', [98, 87, 78])
+let studentOne = new Student('btest-student', [98, 87, 78])
+let studentTow = new Student('Atest-student', [76, 87, 78])
+let studentThree = new Student('ctest-student', [65, 87, 78])
+
+let testArr = [studentOne, studentTow, studentThree]
 
 let students = []
 function createStudent() {
@@ -48,3 +52,31 @@ students.forEach((student) => {
     student.calcAverage()
   )
 })
+
+function findWorstStudent() {
+  studentSorted = []
+  students.forEach((student) => {
+    let newStudent = {
+      name: student.studentName,
+      average: student.calcAverage(),
+    }
+    studentSorted.push(newStudent)
+  })
+
+  bestStudent = studentSorted[0]
+  worstStudent = studentSorted[studentSorted.length - 1]
+  return `best student is ${bestStudent.name} with avg of ${bestStudent.average}. \n worst student is ${worstStudent.name} with avg of ${worstStudent.average}`
+}
+
+console.log('findWorstStudent:', findWorstStudent())
+function sortByName() {
+  studentSorted = []
+  students.forEach((student) => {
+    name = student.studentName
+    studentSorted.push(name)
+  })
+
+  return studentSorted.sort()
+}
+
+console.log('sortByName:', sortByName())
