c07bd3f95ff33cf7f56873b49fc258dba670a3b2
tal0311 <tal.amit0311@gmail.com>
2022-01-05 20:45:41 +0200

    biggest number array of arrays

--- coding academy prep/54.object.js
+++ coding academy prep/54.object.js
@@ -19,3 +19,20 @@ for (let i = 0; i < pets.length; i++) {
   petName = pets[i].petName
   console.log(petName)
 }
+
+let nums1 = [4, 5, 6]
+let nums2 = [7, 8, 9]
+let nums3 = [1, 3, 43]
+
+let numsArr = [nums1, nums2, nums3]
+
+let biggestNum = 0
+for (let i = 0; i < numsArr.length; i++) {
+  number = numsArr[i]
+  for (let j = 0; j < number.length; j++) {
+    if (number[i] > biggestNum) {
+      biggestNum = number[i]
+    }
+  }
+}
+console.log(biggestNum)
