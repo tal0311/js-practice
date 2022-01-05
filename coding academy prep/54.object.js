e6a1c1fb2f7256afaacd43eb6553119a56325e7b
tal0311 <tal.amit0311@gmail.com>
2022-01-05 20:34:20 +0200

    objects

---
+++ coding academy prep/54.object.js
@@ -0,0 +1,21 @@
+let pets = [
+  {
+    petName: 'luli',
+    age: 4,
+    weight: 20,
+  },
+  {
+    petName: 'puki',
+    age: 2,
+    weight: 15,
+  },
+  {
+    petName: 'shuki',
+    age: 5,
+    weight: 12,
+  },
+]
+for (let i = 0; i < pets.length; i++) {
+  petName = pets[i].petName
+  console.log(petName)
+}
