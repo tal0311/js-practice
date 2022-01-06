a2322bf0483b34fdcbc8a242e321e41d65181252
tal0311 <tal.amit0311@gmail.com>
2022-01-06 15:11:29 +0200

    findAvg

--- coding academy prep/56.multi-d-arrays.js
+++ coding academy prep/56.multi-d-arrays.js
@@ -46,7 +46,7 @@ function findMax(mat, colIdx) {
   let colMax = 0
   for (let i = 0; i < mat.length; i++) {
     const row = mat[i]
-    console.log(row)
+
     for (let j = 0; j < row.length; j++) {
       if (row[j] === row[colIdx] && row[colIdx] > colIdx) {
         colMax = row[colIdx]
@@ -54,4 +54,22 @@ function findMax(mat, colIdx) {
     }
   }
   return colMax
+}
+
+// d.
+console.log('d:', '')
+console.log('findAvg:', findAvg(mat))
+function findAvg(mat) {
+  let sum = 0
+  let divider = []
+  for (let i = 0; i < mat.length; i++) {
+    row = mat[i]
+    for (let j = 0; j < row.length; j++) {
+      const num = row[j]
+      sum += num
+      divider.push(num)
+    }
+  }
+  avg = sum / divider.length
+  return avg
 }
