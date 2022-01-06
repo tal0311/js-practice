e20f6c0760723623264fa199eaa60f228c54cb73
tal0311 <tal.amit0311@gmail.com>
2022-01-06 15:00:43 +0200

    fimdMax

---
+++ coding academy prep/56.multi-d-arrays.js
@@ -0,0 +1,57 @@
+// 55.Fill up a multi-dimensional array with numbers, and then, Write the following
+// functions:
+// a. sumCol(mat, colIdx)
+// b. sumRow(mat, rowIdx)
+// c. findMax(mat, colIdx)
+// d. findAvg(mat)
+// e. sumArea(mat, rowIdxStart, rowIdxEnd, colIdxStart, colIdxEnd)
+
+let one = [10, 20, 30]
+let tow = [40, 50, 60]
+let three = [70, 80, 90]
+
+let mat = [one, tow, three]
+// console.log('mat:', mat)
+
+// a.
+console.log('a:', '')
+sumCol(mat, 0)
+function sumCol(mat, colIndex) {
+  for (let i = 0; i < mat.length; i++) {
+    const rows = mat[i]
+    for (let j = 0; j < rows.length; j++) {
+      if (rows[j] === rows[colIndex]) {
+        console.log(rows[colIndex])
+      }
+    }
+  }
+}
+
+// b.
+console.log('b:')
+sumRow(mat, 1)
+function sumRow(mat, rowIdx) {
+  for (let i = 0; i < mat.length; i++) {
+    const row = mat[rowIdx]
+    if (mat[i] === row) {
+      console.log(row)
+    }
+  }
+}
+
+// c.
+console.log('c:', '')
+console.log('findMax:', findMax(mat, 0))
+function findMax(mat, colIdx) {
+  let colMax = 0
+  for (let i = 0; i < mat.length; i++) {
+    const row = mat[i]
+    console.log(row)
+    for (let j = 0; j < row.length; j++) {
+      if (row[j] === row[colIdx] && row[colIdx] > colIdx) {
+        colMax = row[colIdx]
+      }
+    }
+  }
+  return colMax
+}
