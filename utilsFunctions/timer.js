370bb1be1428f08d200f6a3b0b8fd098b5b13e54
Tal Amit <83287794+tal0311@users.noreply.github.com>
2022-01-25 22:19:59 +0200

    Update timer.js

--- utilsFunctions/timer.js
+++ utilsFunctions/timer.js
@@ -12,3 +12,23 @@ function startTimer() {
     renderTimer()
   }, 10)
 }
+
+
+// better one
+
+function startStopWatch() {
+  gWatchInterval = setInterval(updateWatch, 1)
+  gStartTime = Date.now()
+}
+
+function updateWatch() {
+  var now = Date.now()
+  var time = ((now - gStartTime) / 1000).toFixed(3)
+  var elTime = document.querySelector('.time')
+  elTime.innerText = time
+}
+
+function endStopWatch() {
+  clearInterval(gWatchInterval)
+  gWatchInterval = null
+}
