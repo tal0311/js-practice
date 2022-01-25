085f9db899b408bee1f8d292d5961ccd190fc2d6
Tal Amit <83287794+tal0311@users.noreply.github.com>
2022-01-25 22:37:51 +0200

    Update renderMat.js

--- utilsFunctions/renderMat.js
+++ utilsFunctions/renderMat.js
@@ -61,3 +61,21 @@ function renderBoard() {
   var elBoard = document.querySelector('.board')
   elBoard.innerHTML = strHtml
 }
+
+
+// from pacman project
+function printMat(mat, selector) {
+  var strHTML = '<table border="0"><tbody>'
+  for (var i = 0; i < mat.length; i++) {
+    strHTML += '<tr>'
+    for (var j = 0; j < mat[0].length; j++) {
+      var cell = mat[i][j]
+      var className = 'cell cell-' + i + '-' + j
+      strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
+    }
+    strHTML += '</tr>'
+  }
+  strHTML += '</tbody></table>'
+  var elContainer = document.querySelector(selector)
+  elContainer.innerHTML = strHTML
+}
