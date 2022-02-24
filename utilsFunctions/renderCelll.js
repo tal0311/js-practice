8ed2a1343e03d13d5c1f982ec3ea690be08715fe
Tal Amit <83287794+tal0311@users.noreply.github.com>
2022-02-24 09:30:02 +0200

    Delete utilsFunctions directory

--- utilsFunctions/renderCelll.js
+++
@@ -1,5 +0,0 @@
-function renderCell(location, value) {
-  // Select the elCell and set the value
-  var elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
-  elCell.innerHTML = value
-}
